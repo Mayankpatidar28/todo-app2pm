8219e0d90ff829eadf66fb807c5c97f3e0ec2de0
Mayank patidar <you@example.com>
2024-02-20 00:53:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [todo, setTodo] = useState([])
+  const addTodo = () =>
+  {
+    console.log("Add todo")
+    const todoText = document.getElementById("todoInput").value
+    console.log("todoText:"+todoText)
+    todo.push(todoText)
+    setTodo([...todo])   //=> setTodo (["element 1", "element 2"])
+
+  }
+  console.log("Length of Todo",todo.length)
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input id='todoInput' type='text' placeholder='Add your todo here...'/>
+      <button onClick={addTodo}> Add</button><br/>
+      {todo.map(tempTodo =>
+      {
+        //console.log("tempTodo:"+tempTodo)
+        return <div>{tempTodo}</div>
+      })}      
+    </div>
+  );
+}
+
+export default App;
